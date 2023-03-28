75d567b810de6223f15a159c12c8ec2d2a603225
Vito Palumbo <ze_rat@hotmail.it>
2023-03-29 01:05:16 +0200

    Final commit

---
+++ assets/js/variables.js
@@ -0,0 +1,12 @@
+---
+---
+
+var musicOnIconSrc = "{{"/assets/img/MusicOn.png" | relative_url}}";
+var musicOffIconSrc = "{{"/assets/img/MusicOff.png" | relative_url}}";
+
+var dialogIntroPart1 = "Oh oh, benvenuto giovane avventuriero.\nSe sei giunto fin qui, significa che sei stato convocato a brindare il mio trentesimo compleanno!\nMa avventurarsi senza un invito è pericoloso!\n";
+var dialogIntroPart2 = "Ahimè, l'invito è andato perso, dunque ti chiedo di ricrearlo insieme a me.\nPonimi le domande necessarie:"
+var dialogWhen = "La festa avverrà il 24 Aprile, alle ore 20:00";
+var dialogWhere = "La festa si terrà presso \"Piano - Susci Italiano\", in Via Donato Boscia, 21, 70023 Gioia del Colle. Di seguito la mappa per orientarsi";
+
+var iframeMap = "<iframe src=\"https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12081.079517672857!2d16.9238496!3d40.8000629!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1347a2a2aa502ebf%3A0x64225b5e53a29192!2sPiano%20-%20Susci%20Italiano!5e0!3m2!1sit!2sit!4v1680039257032!5m2!1sit!2sit\" width=\"400\" height=\"200\" style=\"border:0;\" allowfullscreen=\"\" loading=\"lazy\" referrerpolicy=\"no-referrer-when-downgrade\"></iframe>";
